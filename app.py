c9b7534bb71c8db0f2fabb136c0683eaa2311248
Paulo Junior Schüssler <paulojuniorpjs@hotmail.com>
2023-06-29 07:52:28 -0300

    feat: Improving legislators_support_oppose function.

--- app.py
+++ app.py
@@ -10,20 +10,17 @@ def print_csv_data(data):
     print(df.to_string(index=False, header=False))
 
 def legislators_support_oppose(legislators, vote_result):
-    merged_data = pd.merge(legislators, vote_result, left_on='id', right_on='legislator_id')
+
+    merged_data = legislators.merge(vote_result, left_on='id', right_on='legislator_id')
     grouped_data = merged_data.groupby(['legislator_id', 'name', 'vote_type']).size().reset_index(name='vote_count')
+    grouped_data.rename(columns={'legislator_id': 'id'}, inplace=True)
 
-    grouped_data.rename(columns = {'legislator_id':'id'}, inplace = True)
+    supported_bills = grouped_data[grouped_data['vote_type'] == 1].rename(columns={'vote_count': 'num_supported_bills'}).drop(columns=['vote_type'])
+    opposed_bills = grouped_data[grouped_data['vote_type'] == 2].rename(columns={'vote_count': 'num_opposed_bills'}).drop(columns=['vote_type'])
 
-    supported_bills = grouped_data[grouped_data['vote_type'] == 1].rename(columns={'vote_count': 'num_supported_bills'})
-    opposed_bills = grouped_data[grouped_data['vote_type'] == 2].rename(columns={'vote_count': 'num_opposed_bills'})
+    legislators_result = supported_bills.merge(opposed_bills, on=['id', 'name'])
 
-    supported_bills.drop(columns=['vote_type'], inplace=True)
-    opposed_bills.drop(columns=['vote_type'], inplace=True)
-
-    legislators_result = pd.merge(supported_bills, opposed_bills, on=['id', 'name'])
-
-    print(legislators_result)
+    return legislators_result
 
 # Define the CSV file paths
 csv_file_paths = [
@@ -39,4 +36,4 @@ legislators = read_csv_file(csv_file_paths[1])
 vote_result = read_csv_file(csv_file_paths[2])
 votes = read_csv_file(csv_file_paths[3])
 
-legislators_support_oppose(legislators, vote_result)
+legislators_support_oppose_count = legislators_support_oppose(legislators, vote_result)
